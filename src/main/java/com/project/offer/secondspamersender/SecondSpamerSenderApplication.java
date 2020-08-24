c326a0a3e10ef8623d844cc3beee95d16b4f4492
GeorgeTedeev <biruang123@gmail.com>
2020-08-24 07:45:23 +0300

    init

---
+++ src/main/java/com/project/offer/secondspamersender/SecondSpamerSenderApplication.java
@@ -0,0 +1,13 @@
+package com.project.offer.secondspamersender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondSpamerSenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondSpamerSenderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/offer/secondspamersender/SecondSpamerSenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.offer.secondspamersender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondSpamerSenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
